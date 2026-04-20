bab3482fda74277a298b3201be5431c58a3b00dc
Segun Ojo <olusegunkd2017@gmail.com>
2026-04-20 13:33:56 +0100

    feat: all shadcn components, and herowip

---
+++ app/home/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/header"
+import Hero from "@/components/hero"
+
+const Home = () => {
+  return (
+    <div className="px-24 max-w-7xl mx-auto">
+        <Header />
+        <Hero />
+    </div>
+  )
+}
+
+export default Home
